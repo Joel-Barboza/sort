9984637e5215def43054e17ae816efa3c44ac1a1
Joel <joelbp5631@gmail.com>
2022-11-21 15:15:52 -0600

    bubble sort working

--- src/SortApp.jsx
+++ src/SortApp.jsx
@@ -6,10 +6,10 @@ import { getArray } from './helpers/getArray';
 
 export const SortApp = () => {
 
-    const { array, newArray, disableBtn, bubbleSort, mergeSort} = getArray();
+    const { array, newArray, disableBtn, bubbleSort, mergeSort, merge, test} = getArray();
 
     return (
-        <>
+        <div className="container">
             <div id="barsContainer" className="bar-container">
                 {
                     array.map( (value, index) => (
@@ -20,10 +20,13 @@ export const SortApp = () => {
                         ))
                     }
             </div>
-            <button onClick={ newArray } disabled={ disableBtn }>New Array</button>
-            <button onClick={ bubbleSort } disabled={ disableBtn }>Bubble Sort</button> 
-            <button onClick={ mergeSort } disabled={ disableBtn }>Merge Sort</button>
-        </>
+            <div className="btn-container">
+                <button onClick={ newArray } disabled={ disableBtn }>New Array</button>
+                <button onClick={ bubbleSort } disabled={ disableBtn }>Bubble Sort</button> 
+                <button onClick={ () => mergeSort(array, 0, array.length - 1) } disabled={ disableBtn }>Merge Sort</button>
+                <button onClick={ test } disabled={ disableBtn }>test</button>
+            </div>
+        </div>
     )
 }
 
--- src/helpers/getArray.js
+++ src/helpers/getArray.js
@@ -3,13 +3,13 @@ import { useState, useEffect } from 'react';
 
 export const getArray = () => {
     
-    const [disableBtn, setDisableBtn] = useState(false)
+    const [disableBtn, setDisableBtn] = useState(false);
     const [arr, setArr] = useState({array: []});
 
     const { array } = arr;
     const arrayLength = array.length;
-    const speed = 10;
-    const numberOfBars = 50;
+    const speed = 0;
+    const numberOfBars = 50 ;
     const colors = {
         purple: 'rgb(233, 96, 233)',
         green: 'rgb(94, 219, 121)',
@@ -18,20 +18,22 @@ export const getArray = () => {
     
     const newArray = () => {
 
-        const array = []
-        let max = 60;
-        let min = 3;
+        const array = [];
+        let max = 110;
+        let min = 4;
 
         for (let i = 0; i < numberOfBars; i++) {
             array.push(Math.floor(Math.random() * ( max - min ) + min ));
         }
     
         setArr({array});
+        console.log(array)
 
     }
 
     useEffect(() => {
         newArray();
+        
     }, []);
     
     function wait(milisec) {
@@ -39,6 +41,83 @@ export const getArray = () => {
             setTimeout(() => { resolve('') }, milisec);
         })
     }
+
+
+    
+    const swap = (array,i,j) => {
+        let temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+
+    const nextGap = (gap) => {
+
+        if ( gap <= 1 ) return 0;
+        return Math.floor(Math.ceil( gap / 2.0 ));
+        
+    }
+    
+    
+    
+
+
+    const merge = async(array, start, end) => {
+
+        let gap = end - start + 1;
+
+        //console.log('gap', gap)
+        for ( gap = nextGap(gap); gap > 0; gap = nextGap(gap)) {
+           
+            for ( let i = start; i + gap <= end; i++ ) {
+                let j = i + gap;
+                barsContainer.children[ j ].style.background = colors.blue;
+                barsContainer.children[ i ].style.background = colors.blue;
+                await wait(speed * gap)
+                
+
+                if ( array[i] > array[j] ) {
+                    
+                    barsContainer.children[ i ].style.background = colors.purple;
+                    barsContainer.children[ j ].style.background = colors.purple;
+                    swap(array, i, j);
+                    setArr({array});
+
+                }
+                
+                
+            }
+            
+        }
+        
+    }
+
+    const mergeSort = async(ar, s, e) => {
+
+        if ( s === e ) return;
+        
+
+        let mid = Math.floor((s + e) / 2);
+        console.log('1-start', array)
+        //await wait(speed)
+        mergeSort(ar, s, mid);
+        //await wait(speed * 3)
+        console.log('2-first', array)
+        mergeSort(ar, mid + 1, e);
+        
+        //await wait(speed * 3)
+        console.log('3-second', array)
+        merge(ar, s, e)
+        
+        
+    }
+
+
+
+
+
+
+
+
 
     const bubbleSort = async() => {
     
@@ -52,6 +131,7 @@ export const getArray = () => {
             setDisableBtn( true );
             const barsContainer = document.getElementById('barsContainer');
             const iteration = arrayLength - i - 1;
+
             if (!!barsContainer.children[ iteration + 1] === !undefined ) {
                 barsContainer.children[ iteration + 1].style.background = colors.green;
                 barsContainer.children[ iteration ].style.background = colors.blue;
@@ -62,29 +142,19 @@ export const getArray = () => {
             for (let j = 0; j < iteration; j++) {
                 await wait(speed);
                 if (!!barsContainer.children[ iteration ] === !undefined ) {
-
                     barsContainer.children[ j ].style.background = colors.purple;
                     barsContainer.children[ j + 1 ].style.background = colors.purple;
-
                 }
 
                 if ( array[ j ] > array[ j + 1]){
-    
-                    let temp = array[ j ];
-                    array[ j ] = array[ j + 1 ];
-                    array[ j + 1] = temp;
-    
+                    swap(array, j, j + 1)
                 } 
                 
                 setArr({array});
                 
-    
                 if (!!barsContainer.children[ j - 1 ] === !undefined ) {
-
                     barsContainer.children[ j - 1].style.background = colors.blue;
-
                 }
-                
             }
             
             setDisableBtn( false );
@@ -102,15 +172,14 @@ export const getArray = () => {
     
     }
 
-    const mergeSort = () => {
-        console.log('Merge Sort')
-    }
+    
 
     return {
         array,
         newArray,
         disableBtn,
         bubbleSort,
-        mergeSort
+        mergeSort,
+        merge
     }
 }
